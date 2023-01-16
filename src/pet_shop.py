6624159a53dbd1f42d3a5908fb1aff1f930e28d2
Connor Fleming <connor.fleming1992@gmail.com>
2023-01-16 09:58:23 +0000

    Added solutions from homework

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -12,15 +12,14 @@ def add_or_remove_cash(pet_shop, amount):
 def get_or_remove_cash(pet_shop, amount):
     pet_shop['admin']['total_cash'] -= amount
 
-
 def get_pets_sold(pet_shop):
     return pet_shop['admin']['pets_sold']
 
 def increase_pets_sold(pets_sold, amount):
     pets_sold['admin']['pets_sold'] += amount
 
-def get_stock_count(pets):
-    return len(pets['pets'])
+def get_stock_count(pet_shop):
+    return len(pet_shop['pets'])
 
 def get_pets_by_breed(shop, breed):
     num_breed = []
@@ -32,6 +31,16 @@ def get_pets_by_breed(shop, breed):
             
     return num_breed
 
+# OR (solution answer)
+
+# def get_pets_by_breed(pet_shop, breed):
+#     found_pets = []
+#     for pet in pet_shop["pets"]:
+#         if pet["breed"] == breed:
+#             found_pets.append(pet)
+
+#     return found_pets
+
 def find_pet_by_name(pet_shop, pet_name):
     for pets in pet_shop['pets']:
         if pets['name'] == pet_name:
@@ -41,6 +50,12 @@ def remove_pet_by_name(pet_shop, pet_name):
     for pets in pet_shop['pets']:
         if pets['name'] == pet_name:
             pet_shop['pets'].remove(pets)
+
+# OR (solutions answer)
+
+# def remove_pet_by_name(pet_shop, name):
+#     pet_to_delete = find_pet_by_name(pet_shop, name)
+#     pet_shop["pets"].remove(pet_to_delete)
 
 def add_pet_to_stock(pet_shop, new_pet):
     pet_shop['pets'].append(new_pet)
@@ -57,17 +72,8 @@ def get_customer_pet_count(customer):
 def add_pet_to_customer(customer, new_pet):
     return customer['pets'].append(new_pet)
 
-
-
-
 def customer_can_afford_pet(customer, pet):
     return customer['cash'] >= pet['price']
-
-# def customer_can_afford_pet(customer, pet):
-#     if customer['cash'] >= pet['price']:
-#         return True
-#     else:
-#         return False
 
 def sell_pet_to_customer(pet_shop, pet, customer):
     if pet in pet_shop['pets']:
@@ -78,3 +84,11 @@ def sell_pet_to_customer(pet_shop, pet, customer):
             pet_shop['admin']['total_cash'] += pet['price']
             pet_shop['admin']['pets_sold'] += 1
 
+#OR (solutions answer)
+# def sell_pet_to_customer(pet_shop, pet, customer):
+#     if pet != None and customer_can_afford_pet(customer, pet):
+#         remove_pet_by_name(pet_shop, pet["name"])
+#         add_pet_to_customer(customer, pet)
+#         remove_customer_cash(customer, pet["price"])
+#         add_or_remove_cash(pet_shop, pet["price"])
+#         increase_pets_sold(pet_shop, 1)
